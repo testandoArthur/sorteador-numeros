78f6a06602d04c668ec696c7bc5bea47e08c77cc
Arthur Teste <jogador161a@gmail.com>
2024-08-19 00:19:55 -0300

    função incompleta (só commitando)

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear(){
+    let quantidadeDeNumeros = document.getElementById('quantidade').value;
+    let doNumero = document.getElementById('de').value;
+    let ateONumero = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidadeDeNumeros}`);
+    alert(`do Numero: ${doNumero}`);
+    alert(`ate o Numero: ${ateONumero}`);
+    
+}
