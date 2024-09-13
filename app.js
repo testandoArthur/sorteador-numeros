2fa6f6a5ad810f5addaaa493f1e311de60714407
Arthur Correia <arthurc.estudos3@gmail.com>
2024-09-13 11:28:38 -0300

    não repetir numeros sorteados

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ function sortear(){
     
     let sorteados = [];
     let numero;
-
+    // colocar os numeros dentro da lista 
     for (let i = 0; i < quantidadeDeNumeros; i ++) {
         numero = obterNumeroAleatorio(doNumero, ateONumero);
-        sorteados.push(numero);
         
+        // nao repetir os numeros sorteados Ex: (1,1,4,5)
+        while (sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(doNumero,ateONumero)
+        }
+        
+        sorteados.push(numero);
     }
+    // mostrar os numeros sorteados na página
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados }</label>`
 }
