b6eee3ff33b52f6260020e63d98351a15db6d0bf
Arthur Correia <arthurc.estudos3@gmail.com>
2024-09-16 21:04:58 -0300

    função reiniciar()

--- app.js
+++ app.js
@@ -11,16 +11,43 @@ function sortear(){
         
         // nao repetir os numeros sorteados Ex: (1,1,4,5)
         while (sorteados.includes(numero)){
-            numero = obterNumeroAleatorio(doNumero,ateONumero)
+            numero = obterNumeroAleatorio(doNumero,ateONumero);
         }
         
         sorteados.push(numero);
     }
     // mostrar os numeros sorteados na página
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados }</label>`
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados }</label>`;
+
+    // chamar a função depois de clicar no botão sortear.
+    alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// alterar o status / deixar funcional o botao reiniciar.
+function alterarStatusBotao() {
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+    
+    if (botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+       botaoReiniciar.classList.remove('container__botao-desabilitado');
+       botaoReiniciar.classList.add('container__botao');
+    } else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    }
+}
+
+// funcionalidade do botão reiniciar 
+function reiniciar() {
+    let reiniciar = document.getElementById('btn-sortear');
+
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').value.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados }</label>`;
+    alterarStatusBotao();
 }
